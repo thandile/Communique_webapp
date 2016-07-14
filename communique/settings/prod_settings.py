67c30958065a925d2a4ecdc20c18659783cd6847
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-07-14 21:02:18 +0200

    Utilise gzip for static content on production

--- communique/settings/prod_settings.py
+++ communique/settings/prod_settings.py
@@ -9,3 +9,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
