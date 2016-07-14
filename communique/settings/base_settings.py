4f6a130fe12ed98226886d4f00355733300b3c35
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-07-14 21:55:42 +0200

    Temporary fix for file path

--- communique/settings/base_settings.py
+++ communique/settings/base_settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 from django.core.urlresolvers import reverse_lazy
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Application definition
 
