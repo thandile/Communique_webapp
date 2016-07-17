4f6d9386596cd8d8fcdcc462ae71ddba5de5420f
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-07-17 19:51:15 +0200

    Created template to view service

--- support_services/urls.py
+++ support_services/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', ServiceListView.as_view(), name='support_services_service_list'),
+    url(r'^(?P<slug>[-\w]+)/$', ServiceDetailView.as_view(), name='support_services_service_detail'),
 ]
