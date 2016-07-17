73ec9bb549eda52abe32d856f210627612f80d9f
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-07-17 09:46:24 +0200

    Set up dummy tables for services list template

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from .views import DashboardView
+from support_services import urls as support_services_urls
 urlpatterns = [
     url(r'^$', DashboardView.as_view(), name='dashboard_home'),
+    url(r'^services/', include(support_services_urls)),
 ]
