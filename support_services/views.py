18001ba3ecde66db2d6834e15e80487867ad7486
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-07-18 15:54:00 +0200

    Created and configured CreateView for Service model

--- support_services/views.py
+++ support_services/views.py
@@ -1,7 +1,9 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 from .models import Service
+from .forms import ServiceForm
 
 class ServiceListView(ListView):
     """
@@ -13,8 +15,16 @@ class ServiceListView(ListView):
 
 class ServiceDetailView(DetailView):
     """
-    A detail view of the support service model.
+    A detail view for the support service model.
     """
     model = Service
     template_name = 'support_services/service_view.html'
     context_object_name = 'service'
+
+class ServiceCreateView(CreateView):
+    """
+    A create view for the support service model.
+    """
+    form_class = ServiceForm
+    model = Service
+    template_name = 'support_services/service_form.html'
