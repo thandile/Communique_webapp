249f4ef036ec489ca276d149d385b0736e0c08dc
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-07-19 11:01:33 +0200

    Configured create service url

--- support_services/urls.py
+++ support_services/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', ServiceListView.as_view(), name='support_services_service_list'),
-    url(r'^(?P<slug>[-\w]+)/$', ServiceDetailView.as_view(),
-        name='support_services_service_detail'),
     url(r'^create/$', ServiceCreateView.as_view(),
         name='support_services_service_create'),
+    url(r'^(?P<slug>[-\w]+)/$', ServiceDetailView.as_view(),
+        name='support_services_service_detail'),
 ]
