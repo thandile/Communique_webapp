89feddf5e987652ca790692448863c1967c82fea
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-07-19 13:21:11 +0200

    Added widget_tweaks app

--- communique/base_settings.py
+++ communique/base_settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget-tweaks',
     'user',
     'dashboard',
     'support_services',
