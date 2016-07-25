effabae016b962192dd7853288ee962531edb3f5
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-07-25 15:59:29 +0200

    Setup list template for pilot programs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
 from .views import DashboardView
-from support_services import urls as support_services_urls
+from services import urls as services_urls
+
 urlpatterns = [
     url(r'^$', DashboardView.as_view(), name='dashboard_home'),
-    url(r'^services/', include(support_services_urls)),
+    url(r'^services/', include(services_urls)),
 ]
