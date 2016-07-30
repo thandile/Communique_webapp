56a166e42bcefa4004a586cb3337cd1c10f57194
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-07-30 22:02:54 +0200

    Added test cases for logging in and out templates

--- user/tests/test_views.py
+++ user/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 class UserViewsTestCase(TestCase):
     def setUp(self):
@@ -9,3 +10,12 @@ class UserViewsTestCase(TestCase):
         self.assertFalse(self.client.login(username='dumb', password='secret'))
         self.assertTrue(self.client.login(username='jon_snow',
             password='p@55words'))
+
+    def test_login_view_template(self):
+        response = self.client.get(reverse('user_login'))
+        self.assertTemplateUsed(response, 'user/login.html')
+
+    def test_logout_view_template(self):
+        self.client.login(username='jon_snow', password='p@55words')
+        response = self.client.get(reverse('user_logout'))
+        self.assertTemplateUsed(response, 'user/logout.html')
