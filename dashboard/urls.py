afd9e440592c2cf27607012c79aa034b4ddfc543
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-07-31 17:28:26 +0200

    Setup view to list user accounts

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from .views import DashboardView
 from services import urls as services_urls
+from user import urls as user_urls
 
 urlpatterns = [
     url(r'^$', DashboardView.as_view(), name='dashboard_home'),
     url(r'^services/', include(services_urls)),
+    url(r'^user/', include(user_urls)),
 ]
