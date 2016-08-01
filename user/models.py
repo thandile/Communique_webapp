904b3b87160216e968e712b3d305b76f370a464f
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-01 21:19:27 +0200

    Created proxy model for a system user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CommuniqueUser(User):
+    """
+    A proxy model for the default User model, adding extra methods.
+    """
+    class Meta:
+        proxy = True
+
+    def get_absolute_url(self):
+        pass
+
+    def get_update_url(self):
+        pass
+
+    def get_deactivate_url(self):
+        pass
