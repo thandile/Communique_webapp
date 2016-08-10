a27bcca79ef0cbb15888933c8107837a96b8692d
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-10 22:55:40 +0200

    Setup templates for password reset

--- communique/base_settings.py
+++ communique/base_settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'send_email.apps.SendEmailConfig',
     'widget_tweaks',
     'rest_framework',
     'api',
