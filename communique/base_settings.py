5f555d9bb1874b17b637015f3d064617460ea83f
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-10 23:00:02 +0200

    Set up dummy email credentials

--- communique/base_settings.py
+++ communique/base_settings.py
@@ -84,6 +84,14 @@ LOGIN_REDIRECT_URL = reverse_lazy('dashboard_home')
 LOGIN_URL = reverse_lazy('user_login')
 LOGOUT_URL = reverse_lazy('user_logout')
 
+# Email settings
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+
 # REST framework settings
 # http://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
