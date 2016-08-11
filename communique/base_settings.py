5667103622212d7ae51548a9363b003fb53ef8d2
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-11 10:20:16 +0200

    Updated settings for debug email server

--- communique/base_settings.py
+++ communique/base_settings.py
@@ -84,8 +84,9 @@ LOGIN_URL = reverse_lazy('user_login')
 LOGOUT_URL = reverse_lazy('user_logout')
 
 # Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
+EMAIL_PORT = 25
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
