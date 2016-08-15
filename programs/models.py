8d09c93ad8d0b292a389dabed1a24b1b843b71c2
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-15 22:20:02 +0200

    Created program model

--- programs/models.py
+++ programs/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Program(models.Model):
+    """
+    A class representing a program/initiative to provide a service to patients.
+    """
+    name = models.CharField(verbose_name="Program name", unique=True, max_length=100)
+    description = models.TextField(verbose_name="Description")
+    date_created = models.DateField(auto_now_add=True)
+    date_last_modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name.title()
+
+    def get_absolute_url(self):
+        pass
+
+    def get_update_url(self):
+        pass
+
+    def get_archive_url(self):
+        pass
