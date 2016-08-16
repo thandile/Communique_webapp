73c4bc2afd534d1ab49ca8ed84263010e27c2b5d
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-16 22:07:30 +0200

    Add descriptive text to is_open field for program

--- programs/models.py
+++ programs/models.py
@@ -10,7 +10,7 @@ class Program(models.Model):
     description = models.TextField(verbose_name="Description")
     date_created = models.DateField(auto_now_add=True)
     date_last_modified = models.DateField(auto_now=True)
-    is_open = models.BooleanField(default=True, help_text='Is this program ongoing or closed?')
+    is_open = models.BooleanField(verbose_name="Is open", default=True, help_text='Is this program ongoing or closed?')
 
     def __str__(self):
         return self.name.title()
