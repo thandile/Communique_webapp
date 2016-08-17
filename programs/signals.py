5f9bf77e1fbc94e25cd9fa5d8723db8f920a39fa
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-17 22:22:30 +0200

    Updated program signals to log name of program

--- programs/signals.py
+++ programs/signals.py
@@ -18,7 +18,8 @@ def post_program_save_callback(sender, **kwargs):
             description_str = "{0} opened a new program titled '{1}'.".format(program.created_by.get_full_name(),
                                                                               program.name)
             UserActivity.objects.create(action=UserActivity.OPEN, actor=program.created_by, object_name="Program",
-                                        object_url=program.get_absolute_url(), description=description_str)
+                                        object_url=program.get_absolute_url(), object_identifier=program.name,
+                                        description=description_str)
     else:
         # program has been updated, yet to discern what type of update i.e closed
         if program.last_modified_by:
@@ -26,5 +27,5 @@ def post_program_save_callback(sender, **kwargs):
                                                                           program.name)
             UserActivity.objects.create(action=UserActivity.UPDATE, actor=program.last_modified_by,
                                         object_name="Program", object_url=program.get_absolute_url(),
-                                        description=description_str)
+                                        object_identifier=program.name, description=description_str)
 
