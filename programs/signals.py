df484652ff9de771dc9a3d2118bd2ea28dc46b98
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-17 20:10:15 +0200

    Setup signals to handle program save

---
+++ programs/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Program
+
+
+@receiver(post_save, sender=Program)
+def post_program_save_callback(sender, **kwargs):
+    """
+    Records the user that has created or updated a program.
+    """
+    if kwargs['created']:
+        print('A new object is created')
