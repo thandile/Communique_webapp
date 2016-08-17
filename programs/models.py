1f162aafc5dfbe3596f89601b767f28e5dfd613b
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-17 15:43:23 +0200

    Added created_by and modified_by fields to Program

--- programs/models.py
+++ programs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+from user.models import CommuniqueUser
+
 
 class Program(models.Model):
     """
@@ -11,6 +13,10 @@ class Program(models.Model):
     date_created = models.DateField(auto_now_add=True)
     date_last_modified = models.DateField(auto_now=True)
     is_open = models.BooleanField(verbose_name="Is open", default=True, help_text='Is this program ongoing or closed?')
+    created_by = models.ForeignKey(CommuniqueUser, on_delete=models.SET_NULL, blank=True, null=True,
+                                   related_name='created_programs', related_query_name='created_program')
+    last_modified_by = models.ForeignKey(CommuniqueUser, on_delete=models.SET_NULL, blank=True, null=True,
+                                         related_name='modified_programs', related_query_name='modified_program')
 
     def __str__(self):
         return self.name.title()
