28612d300849386d6ea40b1cf67c919bf5c49234
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-26 08:39:29 +0200

    Setup a serialiser for the Patient model class

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from programs.models import Program
+from patients.models import Patient
 
 
 class ProgramSerializer(serializers.ModelSerializer):
@@ -13,4 +14,18 @@ class ProgramSerializer(serializers.ModelSerializer):
     class Meta:
         model = Program
         fields = ('id', 'name', 'description', 'is_open', 'created_by', 'last_modified_by')
+        read_only_fields = ('date_created', 'date_last_modified',)
+
+
+class PatientSerializer(serializers.ModelSerializer):
+    """
+    A serializer for the Patient model.
+    """
+    created_by = serializers.ReadOnlyField(source='created_by.username')
+    last_modified_by = serializers.ReadOnlyField(source='last_modified_by.username')
+
+    class Meta:
+        model = Patient
+        fields = ('id', 'first_name', 'last_name', 'middle_name', 'birth_date', 'identifier', 'location',
+                  'contact_number', 'reference_health_centre', 'enrolled_programs')
         read_only_fields = ('date_created', 'date_last_modified',)
