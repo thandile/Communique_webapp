22d2a2eafdfcdcbe280eecbff1dc730b6dd8ac37
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-26 11:41:58 +0200

    Created permission to grant edit access of a Profile only to the owner of the Profile

--- api/permissions.py
+++ api/permissions.py
@@ -21,3 +21,16 @@ class IsSuperUser(permissions.BasePermission):
         Checks whether the user making the request is a superuser.
         """
         return request.user.is_superuser
+
+
+class IsProfileOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owner a profile of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return str(obj.pk) == str(request.user.pk)
