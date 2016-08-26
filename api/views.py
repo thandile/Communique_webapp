b0788aa4198e1904457652075f1399417bd1867b
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-26 09:31:20 +0200

    Added viewset for enrollment model

--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 
 from rest_framework import viewsets
 
-from .serializers import ProgramSerializer, PatientSerializer
+from .serializers import ProgramSerializer, PatientSerializer, EnrollmentSerializer
 from .permissions import IsActiveUser
 
 from programs.models import Program
-from patients.models import Patient
+from patients.models import Patient, Enrollment
 
 
 class ProgramViewSet(viewsets.ModelViewSet):
@@ -38,5 +38,18 @@ class PatientViewSet(viewsets.ModelViewSet):
         serializer.save(created_by=self.request.user, last_modified_by=self.request.user)
 
     def perform_update(self, serializer):
-        # save teh user that has made the modification
+        # save the user that has made the modification
         serializer.save(last_modified_by=self.request.user)
+
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    """
+    This endpoint provides calls to CRUD Enrollment models.
+    """
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+    permission_classes = (IsActiveUser,)
+
+    def perform_create(self, serializer):
+        # save the user that is enrolling the patient
+        serializer.save(enrolled_by=self.request.user)
