70d12a66fac6aabca6445cff2eea8c523a2f859a
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-26 09:33:15 +0200

    Update api urls to enrollment viewset

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import url, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import ProgramViewSet, PatientViewSet
+from .views import ProgramViewSet, PatientViewSet, EnrollmentViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'programs', ProgramViewSet)
 router.register(r'patients', PatientViewSet)
+router.register(r'enrollments', EnrollmentViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
