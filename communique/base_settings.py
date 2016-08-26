7d0cf80e26c3fc31a1675cda75d800ad1c0e2c63
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-26 20:26:29 +0200

    Updated the authentication classes for the rest framework

--- communique/base_settings.py
+++ communique/base_settings.py
@@ -98,6 +98,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # REST framework settings
 # http://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated'
     ),
