03617a10e650070a21d3d52499de236a48b1f42b
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-26 20:48:50 +0200

    Removed redundant rest_framework authentication parameters

--- communique/base_settings.py
+++ communique/base_settings.py
@@ -98,13 +98,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # REST framework settings
 # http://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated'
-    ),
     'DATE_FORMAT':"%d-%m-%Y",
     'DATE_INPUT_FORMATS':["%d/%m/%Y","%d-%m-%Y"]
 }
