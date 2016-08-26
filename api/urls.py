ae797e67400d749c0c1f3baf144ed8cc09925267
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-26 10:52:36 +0200

    Added urls for Communique User viewset to the api

--- api/urls.py
+++ api/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import url, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import ProgramViewSet, PatientViewSet, EnrollmentViewSet
+from .views import ProgramViewSet, PatientViewSet, EnrollmentViewSet, CommuniqueUserViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'programs', ProgramViewSet)
 router.register(r'patients', PatientViewSet)
 router.register(r'enrollments', EnrollmentViewSet)
+router.register(r'users', CommuniqueUserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
