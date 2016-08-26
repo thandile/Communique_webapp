b162a755631d52c388703add20f7d039bf7fcee9
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-26 08:48:05 +0200

    Added patient api urls

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import ProgramViewSet
+from .views import ProgramViewSet, PatientViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'programs', ProgramViewSet)
+router.register(r'patients', PatientViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
