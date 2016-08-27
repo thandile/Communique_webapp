c73c0e932e4c2a854aab14d5a3e0585d5f5af067
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-27 14:38:04 +0200

    Added user activity logging for enrollment creation

--- patients/signals.py
+++ patients/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Patient
+from .models import Patient, Enrollment
 
 from user.models import UserActivity
 
@@ -28,3 +28,19 @@ def post_patient_save_callback(sender, **kwargs):
             UserActivity.objects.create(action=UserActivity.UPDATE, actor=patient.last_modified_by,
                                         object_name="Patient", object_url=patient.get_absolute_url(),
                                         object_identifier=patient.__str__(), description=description_str)
+
+
+@receiver(post_save, sender=Enrollment)
+def post_enrollment_save_callback(sender, **kwargs):
+    """
+    Makes a record of the creation and modification of an enrollment
+    """
+    enrollment = kwargs['instance']
+    if kwargs['created']:
+        # enrollment created
+        if enrollment.enrolled_by:
+            description_str = "{0} enrolled patient '{1} in the program '{2}'.".format(
+                enrollment.enrolled_by.get_full_name(), enrollment.patient.__str__(), enrollment.program.name)
+            UserActivity.objects.create(action=UserActivity.CREATE, actor=enrollment.enrolled_by,
+                                        object_name='Enrollment', object_url=enrollment.get_absolute_url(),
+                                        description=description_str)
