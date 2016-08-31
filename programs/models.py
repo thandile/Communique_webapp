f92ec794449bc070b35e9e0af1d09e5168889c00
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-08-31 11:10:13 +0200

    Updated the help_text of the fields for all models

--- programs/models.py
+++ programs/models.py
@@ -7,15 +7,20 @@ class Program(models.Model):
     """
     A class representing a program/initiative to provide a service to patients.
     """
-    name = models.CharField(verbose_name="Program name", unique=True, max_length=100)
-    description = models.TextField(verbose_name="Description")
-    date_created = models.DateField(auto_now_add=True)
-    date_last_modified = models.DateField(auto_now=True)
-    is_open = models.BooleanField(verbose_name="Is open", default=True, help_text='Is this program ongoing or closed?')
+    name = models.CharField(verbose_name="Program name", unique=True, max_length=100,
+                            help_text='The unique title for the program')
+    description = models.TextField(verbose_name="Description", help_text='The purpose of the program')
+    date_created = models.DateField(auto_now_add=True, help_text='The date the program was first saved into the system')
+    date_last_modified = models.DateField(auto_now=True,
+                                          help_text='The most recent date any of the fields of the program was updated')
+    is_open = models.BooleanField(verbose_name="Is open", default=True,
+                                  help_text='The status of the program. If the program is closed, patients cannot be registered to it')
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True,
-                                   related_name='created_programs', related_query_name='created_program')
+                                   related_name='created_programs', related_query_name='created_program',
+                                   help_text='The user that add this program to the system')
     last_modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True,
-                                         related_name='modified_programs', related_query_name='modified_program')
+                                         related_name='modified_programs', related_query_name='modified_program',
+                                         help_text='The user that made the most recent update to any of the program fields')
 
     def __str__(self):
         return self.name.title()
