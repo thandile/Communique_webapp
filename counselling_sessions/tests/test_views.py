31591ab552b2534b2e44cefeac46d0a558e857d5
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-09-04 15:42:48 +0200

    Added test cases for the session type create view

--- counselling_sessions/tests/test_views.py
+++ counselling_sessions/tests/test_views.py
@@ -13,3 +13,15 @@ class CounsellingSessionTypeListViewTestCase(ViewsTestCase):
 
     def test_active_user_access(self):
         self.only_active_user_access_test(self.view_url, self.view_template_name)
+
+
+class CounsellingSessionTypeCreateViewTestCase(ViewsTestCase):
+    """
+    Test cases for the view to create a new session type.
+    """
+    view_name = 'counselling_sessions_type_create'
+    view_template_name = 'counselling_sessions/counselling_session_type_form.html'
+    view_url = reverse(view_name)
+
+    def test_active_user_access(self):
+        self.only_active_user_access_test(self.view_url, self.view_template_name)
