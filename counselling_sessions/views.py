4d1a05966ac5666a3ff3c4aff07ce6355954f8c4
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-09-04 09:31:12 +0200

    Set up view to list counselling types

--- counselling_sessions/views.py
+++ counselling_sessions/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-# Create your views here.
+from .models import *
+
+
+class CounsellingSessionTypeListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    """
+    A view that retrieves all the available session types.
+
+    This view is only available to users that are logged in and are marked as active in the system.
+    """
+    model = CounsellingSessionType
+    template_name = 'counselling_sessions/counselling_session_list.html'
+    context_object_name = 'counselling_session_list'
+
+    def test_func(self):
+        """
+        Checks whether the user is marked as active.
+        :return: True if user is active, false otherwise.
+        """
+        return self.request.user.is_active
+
