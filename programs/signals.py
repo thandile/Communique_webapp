4e8d3d509b59278a882fc42f767e291f138a776b
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-09-14 17:49:40 +0200

    Added utils for the project to log notifications

--- programs/signals.py
+++ programs/signals.py
@@ -1,9 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Program
+from notifications.signals import notify
 
-from user.models import UserActivity
+from .models import Program
 
 
 @receiver(post_save, sender=Program)
@@ -12,20 +12,19 @@ def post_program_save_callback(sender, **kwargs):
     Makes a record of the creation and updating of a Program by a user.
     """
     program = kwargs['instance']
-    if kwargs['created']:
-        # new program has been opened, note user activity for it
-        if program.created_by:
-            description_str = "{0} opened a new program titled '{1}'.".format(program.created_by.get_full_name(),
-                                                                              program.name)
-            UserActivity.objects.create(action=UserActivity.OPEN, actor=program.created_by, object_name="Program",
-                                        object_url=program.get_absolute_url(), object_identifier=program.name,
-                                        description=description_str)
-    else:
-        # program has been updated, yet to discern what type of update i.e closed
-        if program.last_modified_by:
-            description_str = "{0} updated a program titled '{1}'.".format(program.last_modified_by.get_full_name(),
-                                                                          program.name)
-            UserActivity.objects.create(action=UserActivity.UPDATE, actor=program.last_modified_by,
-                                        object_name="Program", object_url=program.get_absolute_url(),
-                                        object_identifier=program.name, description=description_str)
 
+    # check whether the user responsible for saving the object is available
+    if program.last_modified_by:
+        # check whether the object was created or updated
+
+        if kwargs['created']:
+            verb = 'added'
+        else:
+            verb = 'updated'
+
+        summary = "{0} the program: {1}".format(verb, program)
+
+        description = program.description
+
+        notify.send(program.last_modified_by, recipient=program.last_modified_by, verb=summary, action_object=program,
+                    level='success', description=description, target=program)
