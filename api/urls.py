d13141d250e93af98efca0d2301ad9ffc174cf4e
thandile <thandilexipu@gmail.com>
2016-09-16 18:54:57 +0200

    admissions url fix

--- api/urls.py
+++ api/urls.py
@@ -14,7 +14,7 @@ router.register(r'profiles', ProfileViewSet)
 router.register(r'counselling', CounsellingSessionViewSet)
 router.register(r'session', CounsellingSessionTypeViewSet)
 router.register(r'appointments', AppointmentViewSet)
-router.register(r'admissions', AdmissionSerializer)
+router.register(r'admissions', AdmissionsViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
